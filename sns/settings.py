7bde827a5d574cf266c3e50649d6870017202f79
TaikiCode <metoro5151@gmail.com>
2021-02-20 22:51:38 +0900

    setup s3

--- sns/settings.py
+++ sns/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'posts',
     'accounts',
     'widget_tweaks',
+    'storages',
 ]
 
 
@@ -106,9 +107,13 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# media
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# S3の記述
+AWS_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 # ログイン機能
@@ -140,3 +145,5 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
