8e4164136cd086e58257fdb7e43eeed22e058238
TaikiCode <metoro5151@gmail.com>
2021-02-20 23:12:29 +0900

    add aws s3 config

--- sns/settings.py
+++ sns/settings.py
@@ -5,13 +5,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECRET_KEY = 'zj&40=q5_1z%@850w4&w$h4k@lr+^#e5m%s08q(_+kk3q(e6cc'
-
-# DEBUG = True
-
-# ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -108,6 +101,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # S3の記述
+AWS_STORAGE_BUCKET_NAME = 'django-simple-sns'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
+}
+
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
