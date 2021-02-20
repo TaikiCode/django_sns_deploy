b206399a7bb7f05f353efb9e6759dd19ec0bc032
TaikiCode <metoro5151@gmail.com>
2021-02-20 21:45:56 +0900

    delete wsgi whitenoise

--- sns/wsgi.py
+++ sns/wsgi.py
@@ -7,5 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sns.settings')
 
 application = Cling(get_wsgi_application())
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
